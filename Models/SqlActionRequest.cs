56b087b65c0e5fe24acb1e0875ebeb205e34e3ea
prashant <prashantkumar77395@gmail.com>
2026-09-12 22:50:19 +0530

    Code Audit: Remove unused legacy models and verify 0 warnings/errors

--- Models/SqlActionRequest.cs
+++
@@ -1,8 +0,0 @@
-namespace SQLDatabaseScriptGenerator.Models;
-
-public class SqlActionRequest
-{
-    public string SqlInput { get; set; } = string.Empty;
-    public string Action { get; set; } = string.Empty;
-    public string? Requirement { get; set; }
-}
